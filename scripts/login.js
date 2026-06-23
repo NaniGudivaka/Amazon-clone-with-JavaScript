8c1c793ea7125eae28d4ac0fc348106c41d057dd
NaniGudivaka <nanigudivaka0804@gmail.com>
2026-06-23 13:38:14 +0530

    Adding login.html

---
+++ scripts/login.js
@@ -0,0 +1,31 @@
+
+const isLoggedIn = localStorage.getItem('isLoggedIn');
+
+if (isLoggedIn) {
+  window.location.href = 'amazon.html';
+}
+
+
+document.querySelector('.login-btn')
+  .addEventListener('click', () => {
+
+    const phoneNumber =
+      document.querySelector('.phone-input').value;
+
+    const errorText =
+      document.querySelector('.error-text');
+
+    if (phoneNumber.length !== 10) {
+
+      errorText.innerHTML =
+        'Please enter a valid 10 digit number';
+
+      return;
+    }
+
+    errorText.innerHTML = '';
+
+    localStorage.setItem('isLoggedIn', 'true');
+
+    window.location.href = 'amazon.html';
+  });
